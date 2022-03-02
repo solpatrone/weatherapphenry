77be9fcf29961a1951c393821283fb013adb3295
solpatrone <sol.patrone@hotmail.com>
2022-03-02 16:18:28 -0300

    s

--- src/containers/App.js
+++ src/containers/App.js
@@ -19,7 +19,7 @@ function App() {
   function onSearch(ciudad) {
     //Llamado a la API del clima
     fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=${apiKey}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=${apiKey}`
     )
       .then((r) => r.json())
       .then((recurso) => {
