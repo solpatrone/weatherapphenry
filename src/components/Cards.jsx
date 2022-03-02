f6436c2320bf7f4af7a7f3d2084dd14f0289e7f8
solpatrone <sol.patrone@hotmail.com>
2022-03-02 15:32:08 -0300

    ternary op added

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -6,17 +6,23 @@ import Card from "./Card.jsx";
 export default function Cards({ cities, onClose }) {
   return (
     <div className="cards">
-      {cities.map((c) => (
-        <Card
-          key={c.id}
-          max={c.max}
-          min={c.min}
-          name={c.name}
-          img={c.img}
-          id={c.id}
-          onClose={() => onClose(c.id)}
-        />
-      ))}
+      {cities.length > 0 ? (
+        cities.map((c) => (
+          <Card
+            key={c.id}
+            max={c.max}
+            min={c.min}
+            name={c.name}
+            img={c.img}
+            id={c.id}
+            onClose={() => onClose(c.id)}
+          />
+        ))
+      ) : (
+        <div className="noCards">
+          Please search a city to access its weather information.
+        </div>
+      )}
     </div>
   );
 }
